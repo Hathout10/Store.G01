cfd5134550d37dab116b7a283b1e0a8168ba7835
Mohamed Hathout <ma7755111@gmail.com>
2024-12-28 16:57:07 +0200

    cashservise

---
+++ Store.G01.Core/ServicesContract/IcacheService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G01.Core.ServicesContract
+{
+	public interface IcacheService
+	{
+
+		Task SetCasheKeyAsync(string Key, object response, TimeSpan expireTime);
+		Task<string> GetCasheKeyAsync(string key);
+
+	}
+}
